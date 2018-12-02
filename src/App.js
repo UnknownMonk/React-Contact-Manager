ba9126029372cd7c3f4034ee6efac656db122d37
UnknownMonk <smodcartoon@gmail.com>
2018-12-02 16:16:19 -0500

    added react router

--- src/App.js
+++ src/App.js
@@ -4,19 +4,26 @@ import { Provider } from './context';
 
 import Contacts from './components/constacts/Contacts';
 import Header from './components/layout/Header';
+import About from './components/pages/About';
 import AddContact from './components/constacts/AddContact';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <Provider>
-        <div className="App">
-          <Header branding="Contact Manager" />
-          <div className="container">
-            <AddContact />
-            <Contacts />
+        <Router>
+          <div className="App">
+            <Header branding="Contact Manager" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/contact/add" component={AddContact} />
+              </Switch>
+            </div>
           </div>
-        </div>
+        </Router>
       </Provider>
     );
   }
--- src/components/constacts/AddContact.js
+++ src/components/constacts/AddContact.js
@@ -45,6 +45,7 @@ class AddContact extends Component {
       email: '',
       phone: ''
     });
+    this.props.history.push('/');
   };
   onChange = e => this.setState({ [e.target.name]: e.target.value });
   render() {
